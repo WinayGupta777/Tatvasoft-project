b625faa57797c54ef8fce402a5716c107a83f945
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-02-01 19:22:26 +0530

    Project Initialized; Removed some files; Made First component(MainNav);

---
+++ src/components/MainNav.js
@@ -0,0 +1,21 @@
+import logo from "./logo.png";
+
+const NavBar=()=>{
+    return(
+        <div className="navbar">
+            <div className="lside logo">
+                <img src={logo}></img>
+            </div>
+            <div className="rside">
+                <a id="link" href="#">Login</a><span id="liti"></span>
+                <a id="link" href="#">Register</a>
+                <div className="cartbox">
+                    <i class="fa fa-shopping-cart"></i>
+                    <span className="itemTxt">0</span>
+                    <span className="cartTxt">Cart</span>
+                </div>
+            </div>
+        </div>
+    );
+}
+export default NavBar;
