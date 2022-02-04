bfc0141ba9fcbdad8674f259f3ce65ef2036fc51
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-02-04 18:41:39 +0530

    Full control on detail data; counter done; remove btn not working; total cart item not showing at top;

--- src/components/CartCard.js
+++ src/components/CartCard.js
@@ -1,21 +1,29 @@
-const Card=()=>{
+import { useState } from "react";
+
+const Card=(props)=>{
+    const [count,setCount] = useState(1);
+
+    const onPlus=()=>{setCount(count + 1);}
+    const onMinus=()=>{
+        if (count > 1) {setCount(count - 1);}
+    }
     return(
         <div className="card">
             <div className="thumb"></div>
             <div className="details">
                 <div className="flat">
-                    <b><p>Campus Sutra</p></b>
-                    <b><p>500</p></b>
+                    <b><p>{props.itemname}</p></b>
+                    <b><p>{props.rprice}</p></b>
                 </div>
                 <div className="flat2">
                     <p>Cart item name</p>
-                    <div className="flat2"><p id="originPrice">1000</p><b> 50% off</b></div>
+                    <div className="flat2"><p id="originPrice">{props.oprice}</p><b> 50% off</b></div>
                 </div>
                 <div className="flat3">
                     <div className="cntr">
-                        <button>+</button>
-                        <button>1</button>
-                        <button>-</button>
+                        <button onClick={onPlus}>+</button>
+                        <button id="cnt">{count}</button>
+                        <button onClick={onMinus}>-</button>
                     </div>
                     <button id="rm">Remove</button>
                 </div>
--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -7,11 +7,10 @@ const CartPage=()=>{
         <div className="cart">
             <div className="center">
                 <h3>My Shopping Bag  (3 Items)</h3>
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                <Card itemname="Shampoo"  oprice="1000"  rprice="500"/>
+                <Card itemname="Books"  oprice="540"  rprice="150"/>
+                <Card itemname="Ear phone"  oprice="860"  rprice="430"/>
+                <button id="placeorder">Place order</button>
             </div>
         </div>  
         </>
