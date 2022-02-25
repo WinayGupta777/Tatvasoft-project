878712348cdc507996d980d12dd29b98225ae274
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-02-25 21:03:08 +0530

    Login|Register link navigation  done;

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,14 +1,25 @@
+import React from "react";
 import logo from "./logo.png";
+import { useNavigate } from "react-router";
 
 const NavBar=()=>{
+    var navigate = useNavigate();
+    const navigateMe=(flag)=>{
+        if (flag == "login") {
+            navigate("/login");
+        }
+        else {
+            navigate("register");
+        }
+    }
     return(
         <div className="navbar">
             <div className="lside logo">
                 <img src={logo}></img>
             </div>
             <div className="rside">
-                <a id="link" href="#">Login</a><span id="liti"></span>
-                <a id="link" href="#">Register</a>
+                <a id="link" href="" onClick={()=>navigateMe("login")}>Login</a><span id="liti"></span>
+                <a id="link" href="" onClick={()=>navigateMe("register")}>Register</a>
                 <div className="cartbox">
                     <i className="fa fa-shopping-cart"></i>
                     <span className="itemTxt">0</span>
