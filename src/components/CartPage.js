9a9a0d936770aca5993147cf46d94c096672f69b
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-03-23 16:45:22 +0530

    CartPage fetchs details from MongoDB using axios-express

--- src/components/CartPage.js
+++ src/components/CartPage.js
@@ -1,31 +1,48 @@
 import Card from "./CartCard";
 import TitleBar from "./TitleBar";
 import styles from "./CartPage.css"
+import { useState,useEffect } from "react";
+import axios from "axios";
 
-const cartItems=[
-    {
-        "name" : "Shampoo",
-        "original_price" : 1000,
-        "real_price" : 500
-    },
-    {
-        "name" : "Books",
-        "original_price" : 540,
-        "real_price" : 120
-    },
-    {
-        "name" : "Ear phones",
-        "original_price" : 680,
-        "real_price" : 460
-    },
-    {
-        "name" : "Ear phones",
-        "original_price" : 680,
-        "real_price" : 460
-    }
-]
+// const cartItems=[
+//     {
+//         "name" : "Shampoo",
+//         "original_price" : 1000,
+//         "real_price" : 500
+//     },
+//     {
+//         "name" : "Books",
+//         "original_price" : 540,
+//         "real_price" : 120
+//     },
+//     {
+//         "name" : "Ear phones",
+//         "original_price" : 680,
+//         "real_price" : 460
+//     },
+//     {
+//         "name" : "Ear phones",
+//         "original_price" : 680,
+//         "real_price" : 460
+//     }
+// ]
 
 const CartPage=()=>{
+    const [cartItems, setItems] = useState([]);
+    const [status, setStatus] = useState(false);
+
+
+    useEffect(() => {
+        var r = axios.post("http://localhost:8080/getDetails")
+        r.then((r)=>{
+            console.log(r.data.details[0].item_name)
+            setItems(r.data.details);
+        })
+        r.catch((err)=>console.log(err))
+    },[]);
+
+
+
     return(
         <>
         <TitleBar title="Cart Page" />
@@ -33,7 +50,7 @@ const CartPage=()=>{
             <div className="center">
                 <h3>My Shopping Bag  ({cartItems.length} Items)</h3>
                 {cartItems.map(
-                    (i)=><Card itemname={i.name}  oprice={i.original_price}  rprice={i.real_price}/>
+                    (i)=><Card itemname={i.item_name}  oprice={i.original_price}  rprice={i.real_price}/>
                 )}
                 <button id="placeorder">Place order</button>
             </div>
