12ff3eb88fc1fc7f8c2fd67428f9b5935e00065a
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-04-14 00:47:56 +0530

    ProductList coming from DB; Created separate collection for items;

--- src/components/ProList_AllProduct.js
+++ src/components/ProList_AllProduct.js
@@ -1,14 +1,22 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import SingleProduct from "./ProList_SingleProduct";
 const AllProducts=()=>{
+    const [productItems, setProductItems] = useState([]);
+
+    useEffect(() => {
+        var r = axios.get("http://localhost:8080/getProductItems")
+        r.then((r)=>{
+            console.log(r.data.details[0].ItemName)
+            setProductItems(r.data.details);
+        })
+        r.catch((err)=>console.log(err))
+    },[]);
     return(
         <div className="MainBox">
-            <SingleProduct name="Shampoo" rprice="800"/>
-            <SingleProduct name="Shampoo" rprice="800"/>
-            <SingleProduct name="Shampoo" rprice="800"/>
-            <SingleProduct name="Shampoo" rprice="800"/>
-            <SingleProduct name="Shampoo" rprice="800"/>
-            <SingleProduct name="Shampoo" rprice="800"/>
-            <SingleProduct name="Shampoo" rprice="800"/>
+            {productItems.length != 0 ?  productItems.map(
+                    (i, key)=><SingleProduct name={i.ItemName} desc={i.Description}  imgurl={i.Url} oprice={i.O_price} rprice={i.R_price} key={key} />
+            ) : <h1>Fetching Details From Server</h1>}
         </div>
     );
 }
--- src/components/ProList_SingleProduct.js
+++ src/components/ProList_SingleProduct.js
@@ -7,11 +7,11 @@ const SingleProduct=(props)=>{
     return( 
         <div className="productbox">
             <div className="img_pro">
-                <img src="https://assets.myntassets.com/f_webp,dpr_1.0,q_60,w_210,c_limit,fl_progressive/assets/images/8667337/2022/1/27/31ca0fc8-9d39-4b94-bc46-58a2f7d3b4f51643279915303TRESemmeKeratinSmoothShampoo1l1.jpg"></img>
+                <img src={props.imgurl}></img>
             </div>
             <div className="titlebox">{props.name}</div>
             <div className="descbox">
-                Officia amet et in pariatur. Lorem do qui enim elit aute. Nulla do excepteur reprehenderit dolor. Proident officia minim esse proident dolore cupidatat laborum proident.
+                {props.desc}
             </div>
             <div className="pricebox">
                 <div id="one"><p>MRP</p> <p>₹1000</p> <p>20.00% OFF</p></div>
