d0512f786584ea1f6ea4fe47f18e079aa4c91249
WinayGupta777 <vinay.gupta8101@gmail.com>
2022-04-14 01:22:59 +0530

    Card modified; Add2Cart <-> DB mapped; CartPage <-> DB mapped; Button Added in ListingPage;

--- src/components/ProList_SingleProduct.js
+++ src/components/ProList_SingleProduct.js
@@ -2,8 +2,31 @@ import { useState } from "react";
 import { useNavigate } from "react-router";
 import { useDispatch } from "react-redux";
 import { increaseItem, decreaseItem } from "../redux/items/itemActions";
+import axios from "axios";
 
 const SingleProduct=(props)=>{
+    const [state, setState]= useState(true);
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+
+
+    const added2cart=()=>{
+        setState(!state);
+        dispatch(increaseItem());
+        
+        axios.post("http://localhost:8080/putCartItems",{
+            item_name: props.name,
+            o_price: props.oprice,
+            r_price: props.rprice
+        })
+        .then((r)=>console.log(r.data))
+        .catch((err)=>console.log(err));
+    }
+    const removed=()=>{
+        setState(!state);
+        dispatch(decreaseItem());
+    }
     return( 
         <div className="productbox">
             <div className="img_pro">
@@ -17,35 +40,17 @@ const SingleProduct=(props)=>{
                 <div id="one"><p>MRP</p> <p>₹1000</p> <p>20.00% OFF</p></div>
                 <div id="two"><p>₹ {props.rprice}</p></div>
             </div>
-            <FlipButton />
+            {/* <FlipButton /> */}
+            { state==true ?
+                <div className="add2cart">
+                    <button onClick={added2cart}>ADD TO CART</button>
+                </div>    
+                : 
+                <div className="add2cart">
+                    <button onClick={removed}>Remove</button>
+                </div>
+            } 
         </div>
     );
 }
 export default SingleProduct;   
-
-const FlipButton=(props)=>{
-    const [state, setState]= useState(true);
-    const navigate = useNavigate();
-    const dispatch = useDispatch()
-    const added2cart=()=>{
-        setState(!state);
-        dispatch(increaseItem());
-    }
-    const removed=()=>{
-        setState(!state);
-        dispatch(decreaseItem());
-    }
-    return(
-        <div>
-        { state==true ?
-        <div className="add2cart">
-          <button onClick={added2cart}>ADD TO CART</button>
-        </div>    
-        : 
-        <div className="add2cart">
-            <button onClick={removed}>Remove</button>
-        </div>
-        }  
-        </div>
-    );
-}
--- src/components/ProductListingPage.js
+++ src/components/ProductListingPage.js
@@ -2,13 +2,26 @@ import TitleBar from "./TitleBar";
 import AllProducts from "./ProList_AllProduct";
 import SortBy from "./ProList_SortBy";
 import styles from "./ProductListingPage.css"
+import "./RedButton.css"
+import { useNavigate } from "react-router";
 
 const ProductListingPage=()=>{
+    const navigate = useNavigate();
+
     return(
         <div className="ProListing">
             <TitleBar title="Product Listing"/>
             <SortBy />
             <AllProducts />
+
+            {/* This is taken from RedButton */}
+            <div className="button">
+                <button 
+                 style={{height: "50px", width: "200px", fontSize: "24px"}}
+                 className="red"
+                 onClick={()=>navigate("/cartpage")}
+                >Cart Page</button>
+            </div>
         </div>
     );
 }
